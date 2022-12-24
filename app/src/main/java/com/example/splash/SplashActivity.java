23d138c085ef8da09f0ea81d69b1cbac2c3a8157
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2022-12-24 16:11:19 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setNavigationBarColor(ResourcesCompat.getColor(getResources(), android.R.color.holo_blue_dark, getTheme()));
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
